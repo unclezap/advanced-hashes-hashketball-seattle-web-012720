db2c95ed86e0af76cf102ce045d1c71ce209fe67
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-24 10:59:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -98,6 +98,7 @@ def game_hash
   game_hash = {home:{team_name: "Brooklyn Nets",colors: ["Black", "White"],players:b_array},away:{team_name: "Charlotte Hornets",colors:["Turquoise", "Purple"],players:c_array}}
 
 puts b_array
+puts c_array
 return game_hash
 end
 
